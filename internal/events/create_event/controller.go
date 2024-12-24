0fc714a1a7cdeb868111240244a5eeca58f81373
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2024-12-24 20:40:10 -0300

    feat: Add Swagger documentation and update .gitignore for generated files

--- internal/events/create_event/controller.go
+++ internal/events/create_event/controller.go
@@ -7,18 +7,24 @@ import (
 	models "github.com/idylicaro/event-management/internal/models"
 )
 
-// Controlador para criar um evento
+// @Summary Create a new event
+// @Description Create a new event
+// @Tags events
+// @Accept json
+// @Produce json
+// @Success 201 {object} models.Event
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /events [post]
 func CreateEventController(svc CreateEventService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var event models.Event
 
-		// Bind do corpo da requisição para o modelo Event
 		if err := c.ShouldBindJSON(&event); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		// Chama o serviço para criar o evento
 		if err := svc.CreateEvent(&event); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
